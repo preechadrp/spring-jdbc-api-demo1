f5fdf74f5b203d972761d31fdb0993f5d874eb0f
H P <H P@LAPTOP-KASON>
2026-09-12 11:18:34 +0700

    update

--- src/main/java/com/mycom/springjdbcapidemo1/SpringJdbcApiDemo1Application.java
+++ src/main/java/com/mycom/springjdbcapidemo1/SpringJdbcApiDemo1Application.java
@@ -1,13 +1,36 @@
 package com.mycom.springjdbcapidemo1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.mycom.springjdbcapidemo1.model.CustOrder;
+import com.mycom.springjdbcapidemo1.repository.CustOrderRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SpringJdbcApiDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdbcApiDemo1Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner custOrder(CustOrderRepository custOrderRepository) {
+		return (args) -> {
+			log.info("Running CommandLineRunner.....test");
+			var datas = custOrderRepository.findByCustomerName("customer_name" + 2011);
+			if (datas.size() == 0) {
+				log.info("Not found data.");
+				return;
+			}
+			for (CustOrder data : datas) {
+				log.info("custOrder={}", data.toString());
+			}
+		};
 	}
 
 }
