ff8b447dfce48e4936003a2b2dde5eee878224f2
H P <H P@LAPTOP-KASON>
2026-09-19 13:22:49 +0700

    update

--- src/main/java/com/mycom/springjdbcapidemo1/component/CommonProperties.java
+++ src/main/java/com/mycom/springjdbcapidemo1/component/CommonProperties.java
@@ -34,7 +34,7 @@ public class CommonProperties {
 	public void init() {
 		instance = this;//ช่วยให้ code แบบเก่าสามารถดึงไปใช้งานโดยไม่ต้องใช้หลักการ injection ของ spring
 
-		log.info("Common Properties:");
+		log.info("===== Common Properties =====");
 		log.info("Name: {}", name);
 		log.info("DB1 URL: {}", db1Url);
 		log.info("DB1 DriverClassName: {}", db1DriverClassName);
